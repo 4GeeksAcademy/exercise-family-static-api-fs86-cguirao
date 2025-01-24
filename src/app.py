ac594ca0bbcfa244c5d8519a29fa008f130750de
Cristian-svg598 <criguies@gmail.com>
2025-01-24 12:17:50 +0000

    Creacion endpoints

--- src/app.py
+++ src/app.py
@@ -12,9 +12,6 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
-jackson_family = FamilyStructure("Jackson")
-
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -25,20 +22,101 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+from flask import jsonify
+
+
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def member_list():
+    try:
+        members = jackson_family.get_all_members()
+        if not members:
+            return jsonify({"error": "No members found"}), 400
 
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+        response_body = {
+            "family": members
+        }
+        return jsonify(response_body), 200
+
+    except Exception as e:  
+        return jsonify({"error": "Internal server error", "message": str(e)}), 500
 
 
-    return jsonify(response_body), 200
+@app.route('/members/<int:id>', methods=['GET'])
+def get_member(id):
+    try:
+        member = jackson_family.get_member(id)
+
+     
+        if member is None:
+            return jsonify({'error': 'Member not found'}), 400
+
+      
+        response_body = {
+            "id": member["id"], 
+            "first_name": member["first_name"],  
+            "age": member["age"], 
+            "lucky_numbers": member["lucky_numbers"] 
+        }
+        return jsonify(response_body), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    try:
+        member_data = request.get_json()
+
+        if not all(key in member_data for key in ( "first_name", "age", "lucky_numbers")):
+            return jsonify({"error": "Missing required fields"}), 400
+
+        jackson_family.add_member(member_data)
+        return jsonify({"message": "Member added successfully"}), 200
+
+    except Exception as e: 
+        return jsonify({"error": "Internal server error", "message": str(e)}), 500
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    try:
+        result = jackson_family.delete_member(member_id)
+
+        if result==False:
+            return jsonify({"error": "Member not found"}), 400
+
+        return jsonify({"done": True}), 200
+
+    except Exception as e:
+        return jsonify({"error": "Internal server error", "message": str(e)}), 500
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
+    
+   
+    jackson_family = FamilyStructure("Jackson")
+    
+  
+    jackson_family.add_member({
+        "first_name": "John Jackson",
+        "age": 33,
+        "lucky_numbers": [7, 13, 22]
+    })
+
+    jackson_family.add_member({
+        "first_name": "Jane Jackson",
+        "age": 35,
+        "lucky_numbers": [10, 14, 3]
+    })
+
+    jackson_family.add_member({
+        "first_name": "Jimmy Jackson",
+        "age": 5,
+        "lucky_numbers": [1]
+    })
+
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
